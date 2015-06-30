564912b689331199a3b66f8420c5aefb73677cb6
cermak <p.cermak@fz-juelich.de>
2015-06-30 14:32:01 +0200

    Added example of the package use

---
+++ example.py
@@ -0,0 +1,24 @@
+from crysfipy.reion import re, susceptibility as susc
+import crysfipy.const as C
+import numpy as np
+
+print "*** CrysFiPy test ***"
+print
+print "Cerium 3+"
+ce = re("Ho", [1,0,0], 
+        [-0.173477508,
+        0.001084591,
+        -0.012701252,
+        -3.34835E-06,
+        0.0000097,
+        ])
+ce.calculate()
+print "{0.J2p1:.0f} energy levels, J = {0.J}, gJ = {0.gJ}\nalpha = {0.Alpha}, beta = {0.Beta}, gamma = {0.Gamma}".format(C.ion("ce"))
+print
+print "Energy levels"
+print '\n'.join(format(f, '.3f') for f in sorted(ce.energy))
+print
+print "Calculation of susceptibility"
+temps = [5,10,50,100,300]
+for T in temps:
+    print "T = {0} K \tchi_CF = {1} uB/T".format(T, susc(ce, T))
